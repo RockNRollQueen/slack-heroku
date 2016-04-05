7d1b294cd2b2528dbe7cfb889e00955f5af42961
MonkAlex <3amepob@gmail.com>
2016-04-05 20:36:15 +0400

    server test

--- manage.py
+++ manage.py
@@ -13,6 +13,7 @@ class Index(object):
         form = web.input()
         print form
         if 'command' in form and 'text' in form and 'user_name' in form:
+          web.header('Content-type', 'application/json')
           return json.dumps({"response_type": "in_channel", "text": "{} slaps {} with large trout.".format(form.user_name, form.text)})
         return form
 
