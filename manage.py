5fa523b00643d179d73d1f3542fbeaaaef54b690
MonkAlex <3amepob@gmail.com>
2016-04-05 21:02:45 +0400

    server test

--- manage.py
+++ manage.py
@@ -12,9 +12,9 @@ class Index(object):
     def GET(self):
         form = web.input()
         print form
-        if 'command' in form and 'text' in form and 'user_name' in form:
+        if 'command' in form and 'text' in form and not form.text.isspace():
           web.header('Content-type', 'application/json')
-          return json.dumps({"response_type": "in_channel", "text": "{} slaps {} with large trout.".format(form.user_name, form.text)})
+          return json.dumps({"response_type": "in_channel", "text": "Slaps {} with large trout.".format(form.text)})
         return form
 
 if __name__ == "__main__":
