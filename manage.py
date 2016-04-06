acc60a1ac4f547408f564f1cc3c3ed7c38039ece
MonkAlex <3AMEPOB@gmail.com>
2016-04-06 09:23:22 +0400

    remove get request

--- manage.py
+++ manage.py
@@ -9,13 +9,6 @@ urls = (
 app = web.application(urls, globals())
 
 class Index(object):
-#    def GET(self):
-#        form = web.input()
-#        if 'command' in form and 'text' in form and form.text.strip():
-#          web.header('Content-type', 'application/json')
-#          return json.dumps({"response_type": "in_channel", "text": "Slaps {} around a bit with a large trout".format(form.text)})
-#        return "Use as '/slap username'"
-
     def POST(self):
         form = web.input()
         if 'command' in form and 'text' in form and form.text.strip():
