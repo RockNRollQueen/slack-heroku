ba33cb942f24c6e538c5ec6b43cb094cd5f01965
MonkAlex <3AMEPOB@gmail.com>
2016-04-06 11:07:27 +0400

    test result page

--- manage.py
+++ manage.py
@@ -18,7 +18,8 @@ class Index(object):
         if 'code' in form and form.code == client:
           raise web.seeother("https://slack.com/oauth/authorize?client_id={}&scope={}".format(client, scope))
         if 'code' in form and form.code != client:
-          raise web.seeother("https://slack.com/api/oauth.access?client_id={}&client_secret={}&code={}".format(client, secret, form.code))
+          return "Installed"
+#          raise web.seeother("https://slack.com/api/oauth.access?client_id={}&client_secret={}&code={}".format(client, secret, form.code))
         if 'error' in form:
           return "Canceled"
         return "Auth must be work from 'Add button'"
