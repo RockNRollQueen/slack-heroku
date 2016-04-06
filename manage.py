2366c47eb8678d431a857e2bd4c9876511cde6ce
MonkAlex <3AMEPOB@gmail.com>
2016-04-06 10:18:51 +0400

    test auth

--- manage.py
+++ manage.py
@@ -10,9 +10,15 @@ urls = (
 app = web.application(urls, globals())
 
 class Index(object):
+    def GET(self):
+        form = web.input()
+        if 'code' in form:
+          raise web.seeother("https://slack.com/oauth/authorize?client_id=10543882263.32313872259&scope=channels%3Aread+chat%3Awrite%3Abot")
+        if 'error' in form:
+          return "Canceled"
+        return "Use as '/slap username'"
+
     def POST(self):
-        print self
-        print web
         form = web.input()
         if 'command' in form and 'text' in form and form.text.strip():
           web.header('Content-type', 'application/json')
